dccca49c99798f501b42f4c57b8d010fdb889201
Sandy Hogan <svern99@aol.com>
2020-06-04 20:35:42 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -7,4 +7,11 @@ describe "conversions" do
       expect(grams).to eq(0.0)
     end
   end
+
+  describe '#ounces_to_grams' do
+    it 'given 1, returns 28.3495' do
+      grams = ounces_to_grams(0)
+      expect(grams).to eq(0.0)
+    end
+  end
 end
